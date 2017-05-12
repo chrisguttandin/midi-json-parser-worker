f6ab3b12cfc9198aeab2e3da1a26af147128da67
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-12 23:40:29 +0200

    increase browserNoActivityTimeout

--- config/karma/config-unit.js
+++ config/karma/config-unit.js
@@ -86,6 +86,8 @@ module.exports = (config) => {
 
         config.set({
 
+            browserNoActivityTimeout: 20000,
+
             browsers: [
                 'ChromeSauceLabs',
                 'FirefoxSauceLabs',
