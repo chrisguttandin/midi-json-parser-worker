1958eb86f4bc07d3918ac6e294c5a45b76254274
chrisguttandin <chrisguttandin@media-codings.com>
2017-08-28 12:08:53 +0200

    chore(config): update rollup config

--- config/rollup/bundle.js
+++ config/rollup/bundle.js
@@ -1,13 +1,18 @@
 import babel from 'rollup-plugin-babel';
 
 export default {
-    dest: 'build/es5/bundle.js',
-    entry: 'build/es2015/module.js',
-    format: 'iife',
-    moduleName: 'midiJsonParserWorker',
+    input: 'build/es2015/module.js',
+    output: {
+        file: 'build/es5/bundle.js',
+        format: 'iife',
+        name: 'midiJsonParserWorker'
+    },
     plugins: [
         babel({
             exclude: 'node_modules/**',
+            plugins: [
+                'external-helpers'
+            ],
             presets: [
                 [
                     'es2015',
