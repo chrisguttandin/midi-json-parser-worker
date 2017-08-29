db2c0098524d72dcba0a66e8458d5b12ce51d3af
chrisguttandin <chrisguttandin@media-codings.com>
2017-08-29 15:03:33 +0200

    apply linting rules

--- src/helpers/stringify.ts
+++ src/helpers/stringify.ts
@@ -2,9 +2,9 @@
  * This function turns a part of a given ArrayBuffer into a String.
  */
 export const stringify = (dataView: DataView, offset = 0, length = dataView.byteLength - (offset - dataView.byteOffset)) => {
-    offset += dataView.byteOffset;
+    const byteOffset = offset + dataView.byteOffset;
 
-    const array = new Uint8Array(dataView.buffer, offset, length);
+    const array = new Uint8Array(dataView.buffer, byteOffset, length);
 
     return String.fromCharCode.apply(null, array);
 };
