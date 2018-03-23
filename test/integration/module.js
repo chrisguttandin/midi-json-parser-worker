898cd4fe04c879a0d5cab9a32ef8855981b1738f
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-03-23 16:00:17 +0100

    test: fix typo in comment

--- test/integration/module.js
+++ test/integration/module.js
@@ -3,7 +3,7 @@ import { loadFixtureAsArrayBuffer, loadFixtureAsJson } from '../helper/load-fixt
 describe('module', () => {
 
     afterEach((done) => {
-        // @todo This is a optimistic fix to prevent the famous 'Some of your tests did a full page reload!' error.
+        // @todo This is an optimistic fix to prevent the famous 'Some of your tests did a full page reload!' error.
         setTimeout(done, 1000);
     });
 
