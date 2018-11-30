a3ccb026515fefdd1e93617a033fcb348d70e957
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-11-30 16:36:00 +0100

    fix: cast typed array to number[]

--- src/helpers/stringify.ts
+++ src/helpers/stringify.ts
@@ -6,5 +6,6 @@ export const stringify = (dataView: DataView, offset = 0, length = dataView.byte
 
     const array = new Uint8Array(dataView.buffer, byteOffset, length);
 
-    return String.fromCharCode.apply(null, array);
+    // String.fromCharCode() does normally expect numbers but it can also handle a typed array.
+    return String.fromCharCode.apply(null, <number[]> (<any> array));
 };
