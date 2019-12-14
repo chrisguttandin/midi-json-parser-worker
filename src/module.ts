efe003d4430aa0d35fabf9065bc1c907c1047e3d
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-12-14 15:20:35 +0100

    build(config): update tsconfig file

--- src/module.ts
+++ src/module.ts
@@ -2,8 +2,12 @@ import { TWorkerImplementation, createWorker } from 'worker-factory';
 import { IMidiJsonParserWorkerCustomDefinition } from './interfaces';
 import { parseArrayBuffer } from './midi-file-parser';
 
-export * from './interfaces';
-export * from './types';
+/*
+ * @todo Explicitly referencing the barrel file seems to be necessary when enabling the
+ * isolatedModules compiler option.
+ */
+export * from './interfaces/index';
+export * from './types/index';
 
 createWorker<IMidiJsonParserWorkerCustomDefinition>(self, <TWorkerImplementation<IMidiJsonParserWorkerCustomDefinition>> {
     parse: ({ arrayBuffer }) => {
